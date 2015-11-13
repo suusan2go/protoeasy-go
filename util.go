18983f47688a630ad72156522514c2bb82d9e46f
Peter Edge <peter.edge@gmail.com>
2015-11-13 18:03:34 +0100

    util work

--- util.go
+++ util.go
@@ -6,29 +6,40 @@ import (
 )
 
 var (
-	googleProtobufFiles = []string{
-		"any.proto",
-		"api.proto",
-		"duration.proto",
-		"empty.proto",
-		"field_mask.proto",
-		"source_context.proto",
-		"struct.proto",
-		"timestamp.proto",
-		"type.proto",
-		"wrappers.proto",
-	}
-
-	googleProtobufGoModifierOptions = newGoModifierOptions(
-		"google/protobuf",
-		googleProtobufFiles,
-		"go.pedge.io/google-protobuf",
+	defaultGoModifierOptions = mergeStringStringMaps(
+		newGoModifierOptions(
+			"google/protobuf",
+			[]string{
+				"any.proto",
+				"api.proto",
+				"duration.proto",
+				"empty.proto",
+				"field_mask.proto",
+				"source_context.proto",
+				"struct.proto",
+				"timestamp.proto",
+				"type.proto",
+				"wrappers.proto",
+			},
+			"go.pedge.io/google-protobuf",
+		),
+		map[string]string{
+			"google/protobuf/descriptor.proto": "github.com/golang/protobuf/protoc-gen-go/descriptor",
+		},
 	)
+
+	defaultGoPathRelativeIncludes = []string{
+		"src",
+		"src/github.com/gengo/grpc-gateway/third_party/googleapis",
+	}
 )
 
-type keyValue struct {
-	key   string
-	value string
+func getGoPathIncludes(goPath string, relativeIncludes []string) []string {
+	includes := make([]string, len(relativeIncludes))
+	for i, relativeInclude := range relativeIncludes {
+		includes[i] = filepath.Join(goPath, relativeInclude)
+	}
+	return includes
 }
 
 func newGoModifierOptions(dir string, files []string, goPackage string) map[string]string {
@@ -42,4 +53,14 @@ func newGoModifierOptions(dir string, files []string, goPackage string) map[stri
 
 func newGoModifierOption(file string, goPackage string) (string, string) {
 	return fmt.Sprintf("M%s", file), goPackage
+}
+
+func mergeStringStringMaps(maps ...map[string]string) map[string]string {
+	newMap := make(map[string]string)
+	for _, m := range maps {
+		for key, value := range m {
+			newMap[key] = value
+		}
+	}
+	return newMap
 }
